50044ed697587931cdc5cb057239e1612ad5b755
codingmaster675 <topsecretshadowgovernment@gmail.com>
2024-09-22 11:44:47 +0300

    Conditional Statemnts and Datatypes

---
+++ script.js
@@ -0,0 +1,17 @@
+var name="Adam" // String
+var age=21 // Number
+var profession; // Undefined
+var x=null // Null
+
+// document.write(age==21)
+
+var gender="Male"
+ if (gender != "Male"){
+    document.write("Your a Female")
+ }
+ else if(gender=="Male"){
+    document.write("Your a male")
+ }
+ else {
+    document.write("please type your gender again")
+ }
